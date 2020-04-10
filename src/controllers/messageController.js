f5b860992f2547f2c9658bf8fec18a5207c62562
Jonathan Nolasco <jnolascob@gmail.com>
2020-04-09 21:57:18 -0500

    Add status callback

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -2,7 +2,7 @@ const Twilio = require('twilio');
 const { MessagingResponse } = require('twilio').twiml;
 
 const accountSid = 'AC26f11ce0f4d930cfa4c99eb299980a0a'; // Your Account SID from www.twilio.com/console
-const authToken = 'a196c0c15738e8188bbccf4635f09208'; // Your Auth Token from www.twilio.com/console
+const authToken = 'c55978a5357a2cc4d5f7e37568581d37'; // Your Auth Token from www.twilio.com/console
 
 function send(req, res) {
   const client = new Twilio(accountSid, authToken);
@@ -25,7 +25,7 @@ function sendMedia(req, res) {
     mediaUrl: ['https://images.unsplash.com/photo-1545093149-618ce3bcf49d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'],
     from: 'whatsapp:+14155238886',
     body: 'Hola ZORRO soy DOAPPS papu!!',
-    to: 'whatsapp:+51933440473',
+    to: 'whatsapp:+51993059945',
   })
     .then((message) => {
       return res.status(200).json(message);
@@ -38,8 +38,9 @@ function sendMedia(req, res) {
 
 function sendAnswer(req, res) {
   const { body } = req;
+  console.log('-> Receive message:', body);
   const goodBoyUrl = 'https://images.unsplash.com/photo-1518717758536-85ae29035b6d?'
-  + 'ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+    + 'ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
   let message;
 
   if (body.NumMedia > 0) {
@@ -53,9 +54,21 @@ function sendAnswer(req, res) {
   res.send(message.toString()).status(200);
 }
 
+function listenStatus(req, res) {
+  let to = req.body.to;
+  let fromNumber = req.body.from;
+  let callStatus = req.body.CallStatus;
+  let callSid = req.body.callSid;
+
+  console.log('==> LISTEN...');
+  console.log(to, fromNumber, callStatus, callSid);
+  res.send('Event received');
+}
+
 
 module.exports = {
   send,
   sendMedia,
   sendAnswer,
+  listenStatus,
 };
--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -6,5 +6,6 @@ const Router = express.Router();
 Router.post('/send', messageController.send);
 Router.post('/send-media', messageController.sendMedia);
 Router.post('/send-answer', messageController.sendAnswer);
+Router.post('/listen-status', messageController.listenStatus);
 
 module.exports = Router;
